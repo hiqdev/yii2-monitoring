ca23c303b1ab304060b0910d49096a40b3667424
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2022-06-13 19:25:28 +0300

    Enable NewRelic reporting when available

--- src/Component.php
+++ src/Component.php
@@ -12,10 +12,18 @@ namespace hiqdev\yii2\monitoring;
 
 use Sentry\SentrySdk;
 use Yii;
+use yii\web\User;
+use yii\web\UserEvent;
 
 class Component extends \yii\base\Component
 {
     public function init()
+    {
+        $this->initSentry();
+        $this->initNewRelic();
+    }
+
+    private function initSentry(): void
     {
         if (!class_exists(SentrySdk::class)) {
             return;
@@ -33,5 +41,30 @@ class Component extends \yii\base\Component
             'dsn' => $params['sentry.dsn'],
             'error_types' => E_ALL,
         ]);
+    }
+
+    private function initNewRelic()
+    {
+        if (!extension_loaded('newrelic')) {
+            return;
+        }
+
+        // We do not want to report insignificant errors to New Relic
+        ini_set('newrelic.error_collector.ignore_errors', E_DEPRECATED | E_STRICT | E_NOTICE | E_WARNING);
+        newrelic_capture_params();
+        newrelic_name_transaction(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+
+        // When user is authenticated, add user info
+        if (Yii::$app->has('user')) {
+            $user = Yii::$app->user;
+            $user->on(User::EVENT_AFTER_LOGIN, function (UserEvent $event) {
+                $user = $event->identity;
+                newrelic_add_custom_parameter('user.id', $user->getId());
+
+                if (method_exists($user, 'getLogin')) {
+                    newrelic_add_custom_parameter('user.login', $user->getLogin());
+                }
+            });
+        }
     }
 }
